0be9f8712a35049ce23479ff639daa1bb030dc04
Chad Williams <chadgit13@gmail.com>
2018-09-19 11:53:55 -0400

    Added examples from class

---
+++ ClientSideDemo/js/jQueryDemo.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function(){
+$("h1").click(function(){
+	alert("You clicked me!");
+	$(this).hide();
+    }
+);
+$("p").click(function(){popAlert();});
+$("input").focus(function () {
+  $(this).css("background-color", "#cccccc");
+});
+$("input").blur(function () {
+  $(this).css("background-color", "#ffffff");
+});
+$( "form" ).submit(function(){
+    if ( $("#name").val() !== "Chad"){
+        $("#name").css("background-color","red");
+        alert("Name must be Chad");
+        return false;
+    }else{
+        return true;
+    }
+});
+$("#toggleVisability").click(function(){
+   $("#disappearingText").toggle();
+});
+$("#toggleFade").click(function(){
+   $("#disappearingText").fadeToggle(1000);
+});
+$("#calvin").dblclick(function(){
+	if ($("#calvin").height() < 500){
+  		$("#calvin").animate({
+            left: '250px',
+            height: '+=150px',
+            width: '+=150px'
+        });
+	}else{
+  		$("#calvin").animate({
+            left: '250px',
+            height: '-=300px',
+            width: '-=300px'
+        });
+	}
+});
+});
+
+
